2d45566c7dbade6c1c0bc6de333e24910ed766a5
Jorge Aparicio <japaricious@gmail.com>
2016-04-10 14:35:28 -0500

    don't expand find commands from outside the docker run call

--- ci.sh
+++ ci.sh
@@ -3,7 +3,7 @@
 main() {
     local tag=2016-04-10
 
-    docker run japaric/photon:$tag bash -ex -c "
+    docker run japaric/photon:$tag bash -ex -c '
         rustup default nightly
         cargo install --git https://github.com/japaric/xargo
         git clone --depth 1 https://github.com/japaric/photon
@@ -11,7 +11,7 @@ main() {
         xargo build --release --verbose
         arm-none-eabi-size $(find target/photon/release -type f -executable)
         arm-none-eabi-objdump -CD $(find target/photon/release -type f -executable)
-    "
+    '
 }
 
 main
