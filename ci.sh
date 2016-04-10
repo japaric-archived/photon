48155b841dbe85e684c74a9fb76978d8c67c9a60
Jorge Aparicio <japaricious@gmail.com>
2016-04-10 14:33:22 -0500

    more verbose commands

--- ci.sh
+++ ci.sh
@@ -3,12 +3,12 @@
 main() {
     local tag=2016-04-10
 
-    docker run japaric/photon:$tag sh -c "
+    docker run japaric/photon:$tag bash -ex -c "
         rustup default nightly
         cargo install --git https://github.com/japaric/xargo
         git clone --depth 1 https://github.com/japaric/photon
         cd photon
-        xargo build --release
+        xargo build --release --verbose
         arm-none-eabi-size $(find target/photon/release -type f -executable)
         arm-none-eabi-objdump -CD $(find target/photon/release -type f -executable)
     "
